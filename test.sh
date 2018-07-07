064dda9e8f2de8b06fd0c5441e1dc1362b4da077
Attila Dobosi <dobosiati@gmail.com>
2018-07-07 12:37:25 +1000

    readme file went to root, vim extensions and colors imported, remove test file

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-setno=$(grep -nm1 "set nocompatible" ~/.vimrc |cut -f1 -d:)
-if [ -z "$setno" ]
-then
-	{ echo -n "set nocompatible\n"; cat ~/.vimrc; } > ~/.pre.file
-	mv ~/.pre.file ~/.vimrc
-fi
