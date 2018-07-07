96020e385ce214cf457d29e1e75e2ce725b2f804
Attila Dobosi <dobosiati@gmail.com>
2018-07-07 13:22:10 +1000

    test 4

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ mv ~/.vim/* ~/.my-vim/old-vim
 cp -r ~/.my-vim/new-vim/* ~/.vim
 
 mv ~/.vimrc ~/.my-vim/old-vim/.vimrc
-mv ~/.my-vim/.new-vim/.vimrc ~/.vimrc
+mv ~/.my-vim/new-vim/.vimrc ~/.vimrc
 
 # Dependencies
 sudo apt-get install ctags
