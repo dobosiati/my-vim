e942cae2a3208160e03ba7ea129ce30144177824
Attila Dobosi <dobosiati@gmail.com>
2018-07-07 12:47:15 +1000

    moving vimrc files

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@ git clone https://github.com/dobosiati/my-vim.git ~/my-vim
 mv -r ~/.vim/* ~/.my-vim/old-vim
 cp -r ~/.my-vim/new-vim/* ~/.vim
 
+mv ~/.vimrc ~/.my-vim/old-vim/.vimrc
+mv ~/.vim/.vimrc ~/.vimrc
+
 # Dependencies
 sudo apt-get install ctags
 sudo apt-get install silversearcher-ag
